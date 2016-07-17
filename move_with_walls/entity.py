ab636f2d5c24ebca92fb485327fc92610850bee3
markus <mighty.panders@gmail.com>
2016-07-17 16:34:18 +0200

    put bombs generate grid collision

--- move_with_walls/entity.py
+++ move_with_walls/entity.py
@@ -1,10 +1,18 @@
+from datetime import datetime
+
 import pygame
+
+wall_width = 10
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (50, 50, 255)
+RED = (255, 0, 0)
 
 
 class Player(pygame.sprite.Sprite):
 	def __init__(self, color, x, y):
 		super().__init__()
-		self.image = pygame.Surface([50, 50])
+		self.image = pygame.Surface([20, 20])
 		self.image.fill(color)
 
 		self.rect = self.image.get_rect()
@@ -14,6 +22,11 @@ class Player(pygame.sprite.Sprite):
 		self.changex = 0
 		self.changey = 0
 		self.walls = None
+
+		self.bomb_color = WHITE
+		self.bombs = []
+		self.bomb_max = 1
+		self.bombs_placed = 0
 
 	def changespeed(self, x, y):
 		self.changex += x
@@ -36,6 +49,39 @@ class Player(pygame.sprite.Sprite):
 			elif self.changey < 0:
 				self.rect.top = block.rect.bottom
 
+	def putbomb(self):
+		if (self.bomb_max >= self.bombs_placed):
+			print("bombe hier")
+			newbomb = Bomb(self.bomb_color, self)
+			print(newbomb.rect.x, " ", newbomb.rect.y)
+			self.bombs_placed +=1
+			self.bombs.append(newbomb)
+
+			return self.bombs
+		else:
+			print("keine bombe")
+			return self.bombs
+
+
+class Bomb(pygame.sprite.Sprite):
+	def __init__(self, color, player):
+		super().__init__()
+		self.image = pygame.Surface([10, 10])
+		self.image.fill([255, 255, 255])
+		# self.image.set_colorkey([255,255,255])
+		pygame.draw.ellipse(self.image, color, [0, 0, 10, 10], 2)
+		self.player = player
+		self.rect = self.image.get_rect()
+		self.rect.x = player.rect.x
+		self.rect.y = player.rect.y
+		self.timeplaced = datetime.now()
+
+	def update(self):
+		delta = datetime.now () - self.timeplaced
+		delta = delta.seconds
+		if delta > 3:
+			self.kill()
+			self.player.bombs_placed -=1
 
 class Wall(pygame.sprite.Sprite):
 	def __init__(self, x, y, width, height, color):
@@ -48,12 +94,38 @@ class Wall(pygame.sprite.Sprite):
 		self.rect.y = y
 
 
-def makesomewalls(color):
+def makesomewalls(color, size):
 	wall_list = []
-	wall = Wall(0, 0, 10, 600, color)
+
+	wall = Wall(0, 0, wall_width, size[1], color)
 	wall_list.append(wall)
-	wall = Wall(10, 0, 790, 10, color)
+
+	wall = Wall(0, 0, size[0], wall_width, color)
 	wall_list.append(wall)
-	wall = Wall(10, 200, 100, 10, color)
+
+	wall = Wall(size[0] - wall_width, 0, wall_width, size[1], color)
 	wall_list.append(wall)
+
+	wall = Wall(0, size[1] - wall_width, size[0], wall_width, color)
+	wall_list.append(wall)
+
+	return wall_list
+
+
+def makesomeblocks(color, size, count):
+	wall_list = []
+
+	cHor = (size[0] - (wall_width * 2)) / ((count * 2) + 1)
+	cVert = (size[1] - (wall_width * 2)) / ((count * 2) + 1)
+
+	# print("vert ", cVert, " hor ", cHor)
+
+	for j in range((count * 2)):
+		if j % 2 == 1:  # jede zweite Reihe wird gefüllt
+			for i in range((count * 2)):
+				if i % 2 == 1:  # jede zweite spalte wird gefüllt
+					wall = Wall(wall_width + i * cHor, wall_width + j * cVert,
+					            cHor,
+					            cVert, color)
+					wall_list.append(wall)
 	return wall_list
--- move_with_walls/game.py
+++ move_with_walls/game.py
@@ -6,7 +6,7 @@ import move_with_walls.entity
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (50, 50, 255)
-
+RED = (255, 0, 0)
 # Screen dimensions
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -17,13 +17,14 @@ CLOCK = pygame.time.Clock()
 DONE = False
 all_sprites = pygame.sprite.Group()
 wall_list = pygame.sprite.Group()
-walls = move_with_walls.entity.makesomewalls(BLUE)
+walls = move_with_walls.entity.makesomewalls(BLUE, SCREEN_SIZE)
+walls.append(move_with_walls.entity.makesomeblocks(WHITE, SCREEN_SIZE, 8))
 
 for wall in walls:
 	wall_list.add(wall)
 	all_sprites.add(wall)
 
-player = move_with_walls.entity.Player(WHITE,100, 100  )
+player = move_with_walls.entity.Player(RED, 20, 20)
 player.walls = wall_list
 
 all_sprites.add(player)
@@ -41,6 +42,8 @@ while not DONE:
 				player.changespeed(0, -3)
 			elif event.key == pygame.K_DOWN:
 				player.changespeed(0, 3)
+			if event.key == pygame.K_SPACE:
+				all_sprites.add(player.putbomb())
 
 		elif event.type == pygame.KEYUP:
 			if event.key == pygame.K_LEFT:
